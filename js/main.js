b9b04a993a43e062d2b2c61cb303c8c5c7986f00
Timqa <xocastillian@gmail.com>
2024-01-15 23:08:34 +0600

    adaptive+

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector('.menu__btn')
+const menuClose = document.querySelector('.menu__close')
+const menuList = document.querySelector('.menu__list')
+const menuBg = document.querySelector('.menu--close')
+
+menuBtn.addEventListener('click', () => {
+	menuList.classList.toggle('menu__list--open')
+	menuBg.classList.toggle('menu--open')
+})
+
+menuClose.addEventListener('click', () => {
+	menuList.classList.remove('menu__list--open')
+	menuBg.classList.remove('menu--open')
+})
